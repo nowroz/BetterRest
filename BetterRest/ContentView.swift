fa1c814b324d86f32a2d2325b7dab6801fdfa03e
nowroz <116680399+nowroz@users.noreply.github.com>
2023-06-07 16:21:48 +0600

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,100 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Nowroz Islam on 4/6/23.
+//
+
+import CoreML
+import SwiftUI
+
+struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    private var bedTime: Date {
+        let time = calculateSleep()
+        return time ?? Date.now
+    }
+    
+    private static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    DatePicker("Please pick a time", selection: $wakeUp, displayedComponents: [.hourAndMinute])
+                        .labelsHidden()
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                } header: {
+                    Text("When do you want to wake up?")
+                }
+                
+                Section {
+                    Stepper(sleepAmount.formatted(), value: $sleepAmount, in: 4...12, step: 0.25)
+                } header: {
+                    Text("Desired amount of sleep")
+                }
+                
+                Section {
+                    Picker("Pick the number of coffee cups", selection: $coffeeAmount) {
+                        ForEach(1..<21) { num in
+                            Text(num == 1 ? "1 cup" : "\(num) cups")
+                                .tag(num)
+                        }
+                    }
+                    .labelsHidden()
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                } header: {
+                    Text("Daily coffee intake")
+                }
+                
+                Section {
+                    Text(bedTime.formatted(date: .omitted, time: .shortened))
+                        .font(.largeTitle)
+                        .frame(maxWidth: .infinity)
+                } header: {
+                    Text("Bedtime")
+                }
+            }
+            .navigationTitle("BetterRest")
+        }
+    }
+    
+    func calculateSleep() -> Date? {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let output = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTimeDifference = output.actualSleep
+            let sleepTime = wakeUp - sleepTimeDifference
+            
+            return sleepTime
+        } catch {
+            return nil
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
